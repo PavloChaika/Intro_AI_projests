7df6d9b1cc4bbc456872736dbc8ec8a9f87868fa
Pavlo <chaykivsky@gmail.com>
2023-11-21 15:05:06 +0200

    Comments

--- TicTacToe/Players.py
+++ TicTacToe/Players.py
@@ -1,46 +1,53 @@
 import random
 import time
 
+# Player class, serves as a base class for HumanPlayer and Computer classes
 class Player():
     def __init__(self, turn):
-         self.turn = turn
+        self.turn = turn
 
-    def pass_move(self,game):
+    def pass_move(self, game):
         pass
 
-
+# HumanPlayer class, inherits from Player
 class HumanPlayer(Player):
     def __init__(self, turn):
         super().__init__(turn)
 
     def pass_move(self, game):
+        # Set the current player's turn and listen for a click on the game window
         game.turn = self.turn
         game.window.onclick(game.click)
         game.window.update()
 
+# Computer class, inherits from Player
 class Computer(Player):
     def __init__(self, turn):
         super().__init__(turn)
 
     def pass_move(self, game):
+        # If it's the computer's turn and the next move is allowed
         if game.allow_next == 'Y':
+            # If it's the first move, choose a random position
             if game.count_none_elements() == game.matrix * game.matrix:
                 game.turn = self.turn
-                game.update_table(random.randint(0, game.matrix - 1),
-                                  random.randint(0, game.matrix - 1))
+                game.update_table(random.randint(0, game.matrix - 1), random.randint(0, game.matrix - 1))
                 game.allow_next = 'N'
             else:
+                # Use the minimax algorithm to determine the optimal move
                 coord = self.minimax(game, self.turn)
                 game.turn = self.turn
                 if game.check() is None:
                     game.update_table(coord['x'], coord['y'])
                     game.allow_next = 'N'
 
+    # Minimax algorithm for determining the optimal move
     def minimax(self, state, player):
         max_one = self.turn  # yourself
         other_player = 'o' if player == 'x' else 'x'
         status = state.check()
-        # first we want to check if the previous move is a winner
+
+        # Check if the previous move is a winner
         if status == other_player:
             if other_player == max_one:
                 return {'x': None, 'y': None, 'score': 1 * (state.count_none_elements() + 1)}
@@ -49,20 +56,26 @@ class Computer(Player):
         elif status == 'ніхто':
             return {'x': None, 'y': None, 'score': 0}
 
+        # Initialize scores based on whether it's maximizing or minimizing player's turn
         if player == max_one:
             final = {'x': None, 'y': None, 'score': -100}  # each score should maximize
         else:
             final = {'x': None, 'y': None, 'score': 100}  # each score should minimize
+
+        # Iterate through all possible moves
         for i in range(state.matrix):
             for j in range(state.matrix):
                 if state.table[i][j] is None:
+                    # Simulate a game after making that move
                     state.table[i][j] = player
-                    score = self.minimax(state, other_player)  # simulate a game after making that move
-                    # undo move
+                    score = self.minimax(state, other_player)
+                    # Undo move
                     state.table[i][j] = None
                     score['x'] = i
                     score['y'] = j  # this represents the move optimal next move
-                    if player == max_one:  # X is max player
+
+                    # Update the final move if it leads to a better score
+                    if player == max_one:
                         if score['score'] > final['score']:
                             final = score
                     else:
--- TicTacToe/TicTacToe.py
+++ TicTacToe/TicTacToe.py
@@ -4,9 +4,10 @@ import turtle
 import math
 from Players import HumanPlayer, Computer
 
-
+# Class definition for the TicTacToeGame
 class TicTacToeGame:
-    def __init__(self, mode = 1, size=600):
+    def __init__(self, mode=1, size=600):
+        # Initialize the game parameters
         self.matrix = 3
         self.size = size
         self.X = -size / 2
@@ -18,41 +19,51 @@ class TicTacToeGame:
         self.xo = turtle.Turtle()
         self.window = turtle.Screen()
         self.mode = mode
+
+        # Choose the starting player randomly in mode 3, otherwise, set to None
         if mode == 3:
             self.turn = random.choice(['x', 'o'])
         else:
             self.turn = None
+
+        # Set initial state based on the chosen mode
         if mode == 1:
             self.allow_next = 'N'
         if mode == 2:
             self.allow_next = 'Y'
 
+    # Method to configure the turtle pen
     def set_pen(self):
         self.xo.color(self.color_pen)
         self.xo.pensize(7)
         self.xo.speed(0)
         self.xo.hideturtle()
 
+    # Method to configure the turtle window
     def set_window(self):
         self.window.setup(self.size, self.size)
-        self.window.title("Хрестики нулики")
+        self.window.title("Tic Tac Toe")
         self.window.bgcolor(self.color_window)
 
+    # Method to draw the tic-tac-toe grid
     def draw_net(self):
         for a in range(1, self.matrix):
+            # Draw vertical lines
             self.xo.penup()
             self.xo.goto(self.X + a * self.Tab, self.Y)
             self.xo.pendown()
             self.xo.goto(self.X + a * self.Tab, -self.Y)
 
+            # Draw horizontal lines
             self.xo.penup()
             self.xo.goto(self.X, self.Y - a * self.Tab)
             self.xo.pendown()
             self.xo.goto(-self.X, self.Y - a * self.Tab)
 
+    # Method to handle player click in Human-Human mode
     def click(self, x, y):
-        row = math.floor((-y + self.size/2)/ self.Tab)
-        col = math.floor((x + self.size/2)/ self.Tab)
+        row = math.floor((-y + self.size / 2) / self.Tab)
+        col = math.floor((x + self.size / 2) / self.Tab)
 
         if self.table[row][col] is not None:
             return "Try again"
@@ -61,86 +72,106 @@ class TicTacToeGame:
             self.update_table(row, col)
             self.allow_next = 'Y'
 
+    # Method to count the number of empty elements in the game table
     def count_none_elements(self):
         count = 0
         for row in self.table:
             count += row.count(None)
         return count
 
+    # Method to update the game table after a move
     def update_table(self, row, col):
-
         col_middle = (col * self.Tab + self.Tab / 2) - self.size / 2
         row_middle = (-row * self.Tab - self.Tab / 2) + self.size / 2
         self.xo.penup()
         self.xo.goto(col_middle - self.Tab / 8, row_middle - self.Tab / 8)
 
+        # Write the current player's symbol on the board
         self.xo.write(self.turn, font=('Arial', int(self.Tab / 4)))
 
-        # add to the table
-
+        # Update the game table
         self.table[row][col] = self.turn
 
+        # Toggle player turn in mode 3
         if self.mode == 3:
             if self.turn == 'o':
                 self.turn = 'x'
             else:
                 self.turn = 'o'
 
-        game.final_screen()
+        # Check for a winner and display the final screen
+        self.final_screen()
 
+    # Method to display the winner on the final screen
     def final_screen(self):
         if self.check() is not None:
             time.sleep(1)
             self.xo.penup()
             self.xo.goto(-150, 0)
             self.xo.clear()
-            self.xo.write("Виграли " + self.check(), font=("Arial", 50))
+            self.xo.write("Winner: " + self.check(), font=("Arial", 50))
 
-
+    # Method to check for a winner in the game
     def check(self):
         # Check rows
         for row in self.table:
-            if all(cell == 'o' for cell in row): return 'o'
-            if all(cell == 'x' for cell in row): return 'x'
+            if all(cell == 'o' for cell in row):
+                return 'o'
+            if all(cell == 'x' for cell in row):
+                return 'x'
+
         # Check columns
         for col in range(self.matrix):
-            if all(row[col] == 'o' for row in self.table): return 'o'
-            if all(row[col] == 'x' for row in self.table): return 'x'
+            if all(row[col] == 'o' for row in self.table):
+                return 'o'
+            if all(row[col] == 'x' for row in self.table):
+                return 'x'
 
         # Check main diagonal
-        if all(self.table[i][i] == 'o' for i in range(self.matrix)): return 'o'
-        if all(self.table[i][i] == 'x' for i in range(self.matrix)): return 'x'
+        if all(self.table[i][i] == 'o' for i in range(self.matrix)):
+            return 'o'
+        if all(self.table[i][i] == 'x' for i in range(self.matrix)):
+            return 'x'
 
         # Check anti-diagonal
-        if all(self.table[i][self.matrix - 1 - i] == 'o' for i in range(self.matrix)): return 'o'
-        if all(self.table[i][self.matrix - 1 - i] == 'x' for i in range(self.matrix)): return 'x'
+        if all(self.table[i][self.matrix - 1 - i] == 'o' for i in range(self.matrix)):
+            return 'o'
+        if all(self.table[i][self.matrix - 1 - i] == 'x' for i in range(self.matrix)):
+            return 'x'
 
-        if sum(None in sublist for sublist in self.table) == 0:
+        # Check for a draw
+        if self.count_none_elements() == 0:
             return 'ніхто'
 
-
+# Main part of the script
 print("Choose the mode: 1 - Human-Computer, 2 - Computer-Human, 3 - Human-Human")
 mode = int(input("Enter number 1-3: "))
 
+# Validate the chosen mode and initiate the game
 if mode in [1, 2, 3]:
     game = TicTacToeGame(mode, 600)
     game.set_pen()
     game.set_window()
     game.draw_net()
 
+    # Initialize players based on the chosen mode
     if mode == 1 or mode == 2:
         player1 = HumanPlayer('x')
         player2 = Computer('o')
 
+        # Main game loop
         while game.check() is None:
             player1.pass_move(game)
-            if game.check() is not None: break
+            if game.check() is not None:
+                break
             player2.pass_move(game)
 
+    # Handle Human-Human mode
     if mode == 3:
         game.window.onclick(game.click)
         game.window.listen()
 
+    # Start the game
     game.window.mainloop()
 else:
     print("Wrong number")
